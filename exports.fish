43e44c9c1f78cf22a7177a431aebf9a4ace22432
Lex Tang <lexrus@gmail.com>
2018-10-24 16:27:18 +0800

    add pub path

--- exports.fish
+++ exports.fish
@@ -8,6 +8,7 @@ set -x PATH $PATH $HOME/Documents/google-cloud-sdk/bin 2>&1 | cat - > /dev/null
 set -x PATH $PATH $HOME/Documents/flutter/bin 2>&1 | cat - > /dev/null
 set -x PATH $PATH $HOME/.local/bin 2>&1 | cat - > /dev/null
 set -x PATH $PATH $HOME/Library/Android/sdk/platform-tools 2>&1 | cat - > /dev/null
+set -x PATH $PATH $HOME/.pub-cache/bin 2>&1 | cat - > /dev/null
 
 set -x ANSIBLE_NOCOWS 1
 set -x LC_ALL en_US.UTF-8
