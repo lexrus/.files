0479c89b55d5a06988629dfe01478aab6ea6353a
Lex Tang <lexrus@gmail.com>
2019-04-23 22:37:05 +0800

    Refine path

--- exports.fish
+++ exports.fish
@@ -4,22 +4,32 @@ end
 # See https://github.com/fish-shell/fish-shell/issues/4103
 
 set -x PATH $PATH $HOME/Dropbox/bin 2>&1 | cat - > /dev/null
-set -x PATH $PATH $HOME/Documents/google-cloud-sdk/bin 2>&1 | cat - > /dev/null
-set -x PATH $PATH $HOME/Documents/flutter/bin 2>&1 | cat - > /dev/null
 set -x PATH $PATH $HOME/.local/bin 2>&1 | cat - > /dev/null
-set -x PATH $PATH $HOME/Library/Android/sdk/platform-tools 2>&1 | cat - > /dev/null
-set -x PATH $PATH $HOME/.pub-cache/bin 2>&1 | cat - > /dev/null
-set -x PATH $PATH $HOME/git/arcanist/arcanist/bin 2>&1 | cat - > /dev/null
 
 set -x ANSIBLE_NOCOWS 1
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
 set -x EDITOR nvim
-set -x ANDROID_HOME $HOME/Library/android/sdk
+
+# rbenv
+command --search rbenv >/dev/null; and begin
+  . (rbenv init -|psub)
+end
+
+# SDKs
+set -x PATH $PATH $HOME/SDKs/google-cloud-sdk/bin 2>&1 | cat - > /dev/null
+set -x PATH $PATH $HOME/SDKs/flutter/bin 2>&1 | cat - > /dev/null
+set -x PATH $PATH $HOME/SDKs/arcanist/arcanist/bin 2>&1 | cat - > /dev/null
+set -x PATH $PATH $HOME/.pub-cache/bin 2>&1 | cat - > /dev/null
+
+# Android
+if test -d $HOME/Library/android/sdk
+  set -x ANDROID_HOME $HOME/Library/android/sdk
+  set -x PATH $PATH $HOME/Library/Android/sdk/platform-tools 2>&1 | cat - > /dev/null
+end
 
 # Golang
-
 if test -d $HOME/go
   set -x GOPATH $HOME/go
   set -x GO15VENDOREXPERIMENT 1
@@ -29,17 +39,10 @@ if test -d $HOME/go
 end
 
 # Swiftenv
-
 if test -d $HOME/.swiftenv
   set -x SWIFTENV_ROOT $HOME/.swiftenv 2>&1 | cat - > /dev/null
   set -x PATH $SWIFTENV_ROOT/bin $PATH 2>&1 | cat - > /dev/null
   command --search swiftenv >/dev/null; and begin
     status --is-interactive; and . (swiftenv init -|psub)
   end
-end
-
-# rbenv
-
-command --search rbenv >/dev/null; and begin
-  . (rbenv init -|psub)
 end
