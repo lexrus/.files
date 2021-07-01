06ae0fac634d8bdfc1185fe5493e5d0fc60ddb2e
Lex Tang <lexrus@gmail.com>
2021-07-02 00:31:14 +0800

    clean up fish_prompt

--- config.fish
+++ config.fish
@@ -11,35 +11,13 @@ function fish_greeting
   fortune 2>/dev/null
 end
 
-function _git_branch_name
-  echo (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-end
-
-function _is_git_dirty
-  echo (command git status -s --ignore-submodules=dirty ^/dev/null)
-end
-
 function fish_prompt
-  set -l red (set_color -o red)
-  set -l green (set_color -o green)
-  set -l normal (set_color normal)
-
-  # Show git branch and dirty state
-  if [ (_git_branch_name) ]
-    set -l git_branch (_git_branch_name) ' '
-
-    if [ (_is_git_dirty) ]
-      set git_info $red $git_branch
-    else
-      set git_info $green $git_branch
-    end
-    echo -n -s $git_info $normal
-  end
+  set last_status $status
 
   set_color $fish_color_cwd
-  echo -n (prompt_pwd)
+  printf '%s ' (prompt_pwd)
+
   set_color normal
-  echo -n ' '
 end
 
 function updateall
