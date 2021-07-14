4938261ee4761b8024ba9d5b2affd6ff3e901c1b
Lex Tang <lexrus@gmail.com>
2021-07-14 14:39:11 +0800

    replace setenv PATH with fish_add_path

--- exports.fish
+++ exports.fish
@@ -4,14 +4,13 @@ function setenv
     set -gx $argv
 end
 
-setenv PATH $PATH /usr/local/sbin
-setenv PATH $PATH $HOME/.local/bin 2>&1 | cat - > /dev/null
+fish_add_path /usr/local/sbin
 
 if [ (uname -p) = "arm" ]
-  setenv PATH /opt/homebrew/bin $PATH
-  setenv PATH /opt/homebrew/sbin $PATH
-  setenv PATH /opt/homebrew/opt/openjdk/bin $PATH
-  setenv PATH $PATH /opt/homebrew/opt/icu4c/bin
+  fish_add_path /opt/homebrew/bin
+  fish_add_path /opt/homebrew/sbin $PATH
+  fish_add_path /opt/homebrew/opt/openjdk/bin
+  fish_add_path /opt/homebrew/opt/icu4c/bin
   setenv LDFLAGS $LDFLAGS "-L/opt/homebrew/opt/readline/lib"
   setenv LDFLAGS $LDFLAGS "-L/opt/homebrew/opt/ncurses/lib"
   setenv LDFLAGS $LDFLAGS "-L/opt/homebrew/opt/sqlite/lib"
@@ -42,19 +41,19 @@ end
 setenv RUBYOPT -W0
 
 # SDKs
-setenv PATH $PATH $HOME/SDKs/google-cloud-sdk/bin 2>&1 | cat - > /dev/null
-setenv PATH $PATH $HOME/SDKs/flutter/bin 2>&1 | cat - > /dev/null
-setenv PATH $PATH $HOME/SDKs/arcanist/arcanist/bin 2>&1 | cat - > /dev/null
-setenv PATH $PATH $HOME/.pub-cache/bin 2>&1 | cat - > /dev/null
+fish_add_path $HOME/SDKs/google-cloud-sdk/bin
+fish_add_path $HOME/fvm/default/bin
+fish_add_path $HOME/SDKs/arcanist/arcanist/bin
+fish_add_path $HOME/.pub-cache/bin
 
 # iOSRe
-setenv PATH $PATH /opt/MonkeyDev/bin 2>&1 | cat - > /dev/null
+fish_add_path /opt/MonkeyDev/bin
 setenv MonkeyDevPath /opt/MonkeyDev
 
 # Android
 if test -d $HOME/Library/android/sdk
   setenv ANDROID_HOME $HOME/Library/android/sdk
-  setenv PATH $PATH $HOME/Library/Android/sdk/platform-tools 2>&1 | cat - > /dev/null
+  fish_add_path $HOME/Library/Android/sdk/platform-tools
 end
 
 # Golang
@@ -63,13 +62,13 @@ if test -d $HOME/go
   setenv GO15VENDOREXPERIMENT 1
   setenv GO111MODULE on
   setenv GOPROXY http://localhost:9090
-  setenv PATH $PATH $HOME/go/bin
+  fish_add_path $HOME/go/bin
 end
 
 # Swiftenv
 if test -d $HOME/.swiftenv
   setenv SWIFTENV_ROOT $HOME/.swiftenv 2>&1 | cat - > /dev/null
-  setenv PATH $SWIFTENV_ROOT/bin $PATH 2>&1 | cat - > /dev/null
+  fish_add_path $SWIFTENV_ROOT/bin
   command --search swiftenv >/dev/null; and begin
     status --is-interactive; and . (swiftenv init -|psub)
   end
